786b62e1806881be71ee2f75bff572c5c3447946
P-Draper <110873848+P-Draper@users.noreply.github.com>
2023-06-17 17:19:10 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,22 @@ let monsterArray = []
 fetch("http://localhost3000/monsters")
 .then (response => response.text())
 .then (data => console.log(data))
+
+//load first 50 monsters to page on load
+     //create globally scoped array
+     //GET fetch data and parse to json
+     //add first 50 to array
+     //append array to the container
+//create a form with fields and submit
+     //write HTML
+      //form, label, text, submit
+      //give ids
+     //confirm defer
+     //
+//add an event listener to the submit to
+//add the submitted details to the end of
+//the list on load
+     //create function
+     //add event listener to submit
+     //append the new submit to container
+     //POST the data to the api
